c9d618612b151b16554afc487b4c8fd850caadd1
ahmed <ahmedmahfouzt@gmail.com>
2025-07-06 14:57:19 +0300

    refactor public route table association name for consistency

--- networking/main.tf
+++ networking/main.tf
@@ -62,7 +62,7 @@ resource "aws_route_table" "public_route_table" {
 }
 
 # Public Route Table and Public Subnet Association
-resource "aws_route_table_association" "dev_proj_1_public_rt_subnet_association" {
+resource "aws_route_table_association" "public_rt_subnet_association" {
   count          = length(aws_subnet.public_subnets)
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public_route_table.id
