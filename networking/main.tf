88bdc590382e17d2892e3a1e39a09ab860442869
ahmed <ahmedmahfouzt@gmail.com>
2025-07-06 14:54:03 +0300

    add public route table and subnet association to main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -48,3 +48,22 @@ resource "aws_internet_gateway" "dev_proj_1_public_internet_gateway" {
     Name = "dev-proj-1-igw"
   }
 }
+
+# Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev_proj_1_public_internet_gateway.id
+  }
+  tags = {
+    Name = "dev-proj-1-public-rt"
+  }
+}
+
+# Public Route Table and Public Subnet Association
+resource "aws_route_table_association" "dev_proj_1_public_rt_subnet_association" {
+  count          = length(aws_subnet.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
