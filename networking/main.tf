56613d50f7459e739630931b16eb3dfebf65f4df
ahmed <ahmedmahfouzt@gmail.com>
2025-07-06 14:59:01 +0300

    fix: correct gateway ID reference in public route table

--- networking/main.tf
+++ networking/main.tf
@@ -54,7 +54,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dev_proj_1_public_internet_gateway.id
+    gateway_id = aws_internet_gateway.public_internet_gateway.id
   }
   tags = {
     Name = "public-rt"
