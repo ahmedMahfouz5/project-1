c2887ac982db3ef2ce3695b5a67125c1b4ef92ea
ahmed <ahmedmahfouzt@gmail.com>
2025-07-06 14:43:43 +0300

    add public subnet configuration to main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -1,15 +1,29 @@
 variable "vpc_cidr" {}
 variable "vpc_name" {}
+variable "cidr_public_subnet" {}
+variable "us_availability_zone" {}
 
-output "dev_proj_1_vpc_id" {
-  value = aws_vpc.proj_1_vpc.id
+output "vpc_id" {
+  value = aws_vpc.vpc.id
 }
 
 
 # Setup VPC
-resource "aws_vpc" "proj_1_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
   tags = {
     Name = var.vpc_name
+  }
+}
+
+# Setup public subnet
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.cidr_public_subnet)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = element(var.cidr_public_subnet, count.index)
+  availability_zone = element(var.us_availability_zone, count.index)
+
+  tags = {
+    Name = "dev-proj-public-subnet-${count.index + 1}"
   }
 }
