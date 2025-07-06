30730d282c0e6f03513b5c29b1d4cad963f4538c
ahmed <ahmedmahfouzt@gmail.com>
2025-07-06 14:20:36 +0300

    add VPC variables for CIDR and name configuration

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "Public Subnet CIDR values"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "DevOps Project 1 VPC 1"
+}
