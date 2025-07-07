7d2d3c2e9b9ee8a0dd766a9c953bfcf1360244e4
ahmed <ahmedmahfouzt@gmail.com>
2025-07-07 06:45:45 +0300

    add Jenkins and Terraform installation script

---
+++ jenkins-template/template.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sudo apt-get update
+yes | sudo apt install openjdk-17-jdk-headless
+echo "Waiting for 30 seconds before installing the jenkins package..."
+sleep 30
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+yes | sudo apt-get install jenkins
+sleep 30
+
+echo "Waiting for 30 seconds before installing the Terraform..."
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+
+sudo apt-get install terraform
